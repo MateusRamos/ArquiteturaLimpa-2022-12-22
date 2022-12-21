79d5f31a184b3c4c3682d15c3ea36dcff5da0435
MateusRamos <mateusterra.iff@gmail.com>
2022-12-21 14:35:29 -0300

    Criação da entidade Livros e sua interface

---
+++ src/entities/entitie-livros.ts
@@ -0,0 +1,30 @@
+
+//Interface Livros:
+interface InterLivros {
+    id: string;
+    descricao: string;
+    autor: string;
+    genero: string;
+    codigoBarras: number;
+}
+
+//Classe principal Livros:
+class Livro implements InterLivros {
+
+    id: string;
+    descricao: string;
+    autor: string;
+    genero: string;
+    codigoBarras: number;
+
+    constructor(id:string, descricao:string, autor:string, genero:string, codigoBarras:number) {
+        this.id = id;
+        this.descricao = descricao;
+        this.autor = autor;
+        this.genero = genero;
+        this.codigoBarras = codigoBarras;
+    }
+
+}
+
+export { InterLivros }
